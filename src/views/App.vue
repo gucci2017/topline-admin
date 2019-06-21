65f7185beadc19895160cfc21b2ad3ec6099cc3d
461436235@qq.com <461436235@qq.com>
2019-06-21 09:38:40 +0800

    调整目录

---
+++ src/views/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>aasfasasaa</p>
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  data () {
+    return {}
+  }
+}
+</script>
+<style scoped>
+
+</style>
